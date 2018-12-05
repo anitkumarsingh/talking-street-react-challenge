e93a1d781193d8a70bce3abf87a16e5cdf059d1a
Anitkumarsingh <rockymaster118@gmail.com>
2018-12-05 22:24:43 +0530

    modification

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Header from './Components/Header/Header';
 import './App.css';
 import Cards from './Components/Cards/Cards';
-import Footer from './Components/Footer/Footer';
+// import Footer from './Components/Footer/Footer';
 
 
 class App extends Component {
@@ -10,9 +10,15 @@ class App extends Component {
     return (
       <div className="App">
         <Header/>
-        <h5 className="headerText">Created By Anit Kumar</h5>
         <Cards/>
-        <Footer/>
+        {/* <Footer/> */}
+        <h5 className="headerText"> &copy; 2018, Coded By Anit Kumar</h5>
+          <div class="icons-bar">
+              <a href="https://www.linkedin.com/in/anit-kumar" target="_blank" rel="noopener noreferrer" className="linkedin" id="profile-link"><i className="fa fa-linkedin"></i></a>
+              <a href="https://www.freecodecamp.org/anitkumar" id="profile-link" target="_blank" rel="noopener noreferrer" className="freecodecamp"><i className="fa fa-free-code-camp"></i></a>
+              <a href="https://codepen.io/Anitkumar" className="codepen" target="_blank" rel="noopener noreferrer"><i className="fa fa-codepen"></i></a>
+              <a href="https://github.com/anitkumarsingh" className="github" target="_blank" rel="noopener noreferrer"><i className="fa fa-github"></i></a>
+          </div>
       </div>
     );
   }
