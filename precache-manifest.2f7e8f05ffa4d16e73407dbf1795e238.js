b21df5f63697dd41e3dbe58acee41e438154518e
anitkumarsingh <anikumarsingh99@gmail.com>
2021-05-22 12:08:53 +0530

    Updates

---
+++ precache-manifest.2f7e8f05ffa4d16e73407dbf1795e238.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b972b58831c021aaf6b7",
+    "url": "/Talking-Street-React-Challenge/static/css/main.91957aea.chunk.css"
+  },
+  {
+    "revision": "b972b58831c021aaf6b7",
+    "url": "/Talking-Street-React-Challenge/static/js/main.b972b588.chunk.js"
+  },
+  {
+    "revision": "a336b8d54f8f7b4faa7b",
+    "url": "/Talking-Street-React-Challenge/static/js/1.a336b8d5.chunk.js"
+  },
+  {
+    "revision": "76581169ba534dd57861",
+    "url": "/Talking-Street-React-Challenge/static/js/runtime~main.76581169.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Talking-Street-React-Challenge/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "06e01139f0a9e38171b01ef4addaac4c",
+    "url": "/Talking-Street-React-Challenge/static/media/Darjeeling-Hot-Momos-Colourful-veg-momos-Koramangala-Bangalore.06e01139.jpg"
+  },
+  {
+    "revision": "ab9675871df749288b385e15bfb6f2d9",
+    "url": "/Talking-Street-React-Challenge/static/media/hackerrank-brands.ab967587.svg"
+  },
+  {
+    "revision": "ae7f11139bcffe1b149cdf66459bdc33",
+    "url": "/Talking-Street-React-Challenge/static/media/background1.ae7f1113.jpg"
+  },
+  {
+    "revision": "9bd55df0479490bc6af95edea6799f42",
+    "url": "/Talking-Street-React-Challenge/index.html"
+  }
+];
